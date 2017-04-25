fe7d1c25a4377310fad212609db1051ad67d97ba
hirayama <hirayama@bp.horizon.co.jp>
2017-04-25 16:31:58 +0900

    勉強中

--- client/src/app/chat/chat.component.ts
+++ client/src/app/chat/chat.component.ts
@@ -6,19 +6,38 @@ import { ChatService }       from './chat.service';
   template: `<div *ngFor="let message of messages">
               {{message.text}}
              </div>
-             <input [(ngModel)]="message" /><button (click)="sendMessage()">Send</button>`,
+             <input [(ngModel)]="message" (keypress)="onPress($event.keyCode)" (keyup.enter)="onEnter($event.keyCode)" /><button (click)="sendMessage()">Send</button>`,
   providers: [ChatService]
 })
 export class ChatComponent implements OnInit, OnDestroy {
   messages = [];
   connection;
   message;
+  enter;
   
   constructor(private chatService:ChatService) {}
 
   sendMessage(){
     this.chatService.sendMessage(this.message);
     this.message = '';
+  }
+
+  /* ここの情報さ参考にして、IME変換決定のEnterは無視するようにしてみた */
+  /* https://garafu.blogspot.jp/2015/09/javascript-ime-enter-event.html */
+  onPress(ev){
+    if(ev==13){ // IME変換決定のEnterでは、Keypress発生しないらしいよ
+      this.enter=true;
+    }else{
+      this.enter=false;
+    }
+  }
+  onEnter(ev){
+    console.info(JSON.stringify(ev));
+    if( this.enter ){
+      this.chatService.sendMessage(this.message);
+      this.message = '';   
+    }
+    this.enter=false;
   }
 
   ngOnInit() {
--- client/src/app/chat/chat.service.ts
+++ client/src/app/chat/chat.service.ts
@@ -12,14 +12,18 @@ export class ChatService {
   
   getMessages() {
     let observable = new Observable(observer => {
+      console.info('あたまです');
       this.socket = io(this.url);
       this.socket.on('message', (data) => {
+        console.info('お、データ来た: '+ JSON.stringify( data ));
         observer.next(data);    
       });
       return () => {
+        console.info('いつ呼ばれる？');
         this.socket.disconnect();
       };  
     })     
+    console.info('おしりです');
     return observable;
   }  
 }
